40c94022346fe7b0e2f4b3ad306d03e77493df9a
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-03-03 22:21:27 +0530

    [Siddharth] ADD: Calculate Mean height in a teamjava program

---
+++ MeanHeightInTeam.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MeanHeightInTeam {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0.0;
+        
+        System.out.println("Enter the heights of 11 football players :");
+        for (int i = 0; i < 11; i++) {
+            heights[i] = scanner.nextDouble(); //input heght of players
+            sum += heights[i];
+        }
+        
+        double mean = sum / 11; //mean calculate
+        System.out.println("Mean height of the football team: " + mean);
+    }
+}
